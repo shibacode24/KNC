9d247441c4e3f177c5958ef1d63e0b3fcd7220ce
Daksh-shaikh <darakhshankausarr@gmail.com>
2024-08-23 11:47:14 +0530

    new modifications

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -28,7 +28,7 @@
                         @csrf
                         <div class="col-md-2">
                             <label class="control-label">Select Employee Type<font color="#FF0000">*</font></label>
-                            <select id="employeeTypeSelect" name="employee" class="form-control select">
+                            <select id="employeeTypeSelect" name="emploxyee" class="form-control select">
                                 <option value="">--Select--</option>
                                 <option value="Site-Manager">Site Manager</option>
                                 <option value="Site-Incharge">Site Incharge</option>
@@ -71,7 +71,7 @@
 @endsection
 
 @section('js')
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC1Cz13aBYAbBYJL0oABZ8KZnd7imiWwA4&callback=initMap" async defer></script>
+    {{-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC1Cz13aBYAbBYJL0oABZ8KZnd7imiWwA4&callback=initMap" async defer></script> --}}
 
     <script>
   $(document).ready(function() {
@@ -118,133 +118,6 @@
 
     </script>
 
-{{-- <script>
-    function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 12,
-            center: { lat: 0, lng: 0 } // Placeholder center, will be updated
-        });
-
-        var locations = @json($locations); // Ensure $locations is a collection or array
-        var latestLocation = @json($latestLocation); // Ensure $latestLocation is an object
-
-        if (latestLocation && latestLocation.latitude && latestLocation.longitude) {
-            var latestLatLng = {
-                lat: parseFloat(latestLocation.latitude),
-                lng: parseFloat(latestLocation.longitude)
-            };
-            console.log(latestLatLng);
-
-            map.setCenter(latestLatLng);
-
-            new google.maps.Marker({
-                position: latestLatLng,
-                map: map,
-                title: 'Latest Location'
-            });
-        } else {
-            console.error('Invalid latest location data:', latestLocation);
-        }
-
-        if (Array.isArray(locations) && locations.length) {
-            locations.forEach(function(location) {
-                if (location.latitude && location.longitude) {
-                    new google.maps.Marker({
-                        position: {
-                            lat: parseFloat(location.latitude),
-                            lng: parseFloat(location.longitude)
-                        },
-                        map: map,
-                        title: new Date(location.created_at).toLocaleString()
-                    });
-                } else {
-                    console.error('Invalid location data:', location);
-                }
-            });
-        } else {
-            console.error('No locations data or invalid format:', locations);
-        }
-    }
-
-    $(document).ready(function() {
-        initMap();
-    });
-</script> --}}
-
-
-{{-- <script>
-    function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 12,
-            center: { lat: 0, lng: 0 } // Placeholder center, will be updated
-        });
-
-        var locations = @json($locations); // Ensure $locations is a collection or array
-        var latestLocation = @json($latestLocation); // Ensure $latestLocation is an object
-
-        var pathCoordinates = []; // To store the coordinates for the polyline
-
-        // Define custom icon sizes
-        var iconSize = new google.maps.Size(20, 20); // Width: 20px, Height: 20px
-
-        if (locations.length) {
-            locations.forEach(function(location, index) {
-                var latLng = {
-                    lat: parseFloat(location.latitude),
-                    lng: parseFloat(location.longitude)
-                };
-
-                pathCoordinates.push(latLng);
-
-                new google.maps.Marker({
-                    position: latLng,
-                    map: map,
-                    title: new Date(location.created_at).toLocaleString(),
-                    icon: {
-                        url: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png', // Blue dot for historical locations
-                        scaledSize: iconSize // Apply custom size
-                    }
-                });
-            });
-        }
-
-        if (latestLocation) {
-            var latestLatLng = {
-                lat: parseFloat(latestLocation.latitude),
-                lng: parseFloat(latestLocation.longitude)
-            };
-
-            // Update the map center to the latest location
-            map.setCenter(latestLatLng);
-
-            // Add marker for the latest location with a distinct icon
-            new google.maps.Marker({
-                position: latestLatLng,
-                map: map,
-                title: 'Current Location',
-                // icon:  'http://maps.google.com/mapfiles/ms/icons/red-dot.png', // Red dot for the current location
-
-            });
-        }
-
-        // Draw the polyline
-        if (pathCoordinates.length) {
-            var path = new google.maps.Polyline({
-                path: pathCoordinates,
-                geodesic: true,
-                strokeColor: '#FF0000',
-                strokeOpacity: 1.0,
-                strokeWeight: 2
-            });
-
-            path.setMap(map);
-        }
-    }
-
-    $(document).ready(function() {
-        initMap();
-    });
-</script> --}}
 
 <script>
 
